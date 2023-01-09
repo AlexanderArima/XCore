906f039beb22df0c135d4f38ac0299e9a2183a5a
XieCheng <xiecheng900424@qq.com>
2023-01-09 10:05:21 +0800

    添加ASP.NET Core MVC项目

---
+++ XCore.Web/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XCore.Web.Models;
+
+namespace XCore.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Demo1()
+        {
+            Person person = new Person()
+            {
+                Name = "ZhangSan",
+                Age = 26,
+                Sex = 1,
+                Job = "程序员"
+            };
+
+            return View(person);
+        }
+    }
+}
---
+++ XCore.Web/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XCore.Web.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Sex { get; set; }
+
+        public string Job { get; set; }
+    }
+}
