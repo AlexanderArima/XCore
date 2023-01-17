c053ecb75ab29c7b2867b37415f076aef5bee312
XieCheng <xiecheng900424@qq.com>
2023-01-17 09:20:35 +0800

    添加生成环境和测试环境的配置，自定义服务

---
+++ XCore.WebAPI/LoginService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XCore.WebAPI
+{
+    public class LoginService
+    {
+        public LoginService()
+        {
+            Debug.WriteLine("初始化LoginService服务");
+        }
+
+        /// <summary>
+        /// 验证.
+        /// </summary>
+        /// <returns></returns>
+        public bool Valid()
+        {
+            return true;
+        }
+    }
+}
