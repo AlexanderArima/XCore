be0c1050bbde4e21320296b5ff72a96e434295da
Arima <xiecheng900424@qq.com>
2023-07-27 15:54:40 +0800

    添加拼图验证码的Demo

---
+++ XCore.Web/Controllers/VCodePuzzleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XCore.Web.Controllers
+{
+    public class VCodePuzzleController : Controller
+    {
+        private readonly ILogger<VCodePuzzleController> _logger;
+
+        public VCodePuzzleController(ILogger<VCodePuzzleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Create()
+        {
+            return "Create";
+        }
+
+        public string Check()
+        {
+            return "Check";
+        }
+    }
+}
