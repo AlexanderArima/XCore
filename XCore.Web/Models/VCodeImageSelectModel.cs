81d21e3aac3878629486a63c6316ae2899f40fc1
Arima <xiecheng900424@qq.com>
2023-08-16 10:18:50 +0800

    添加图片选择验证码的Demo

---
+++ XCore.Web/Models/VCodeImageSelectModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XCore.Web.Models
+{
+    public class VCodeImageSelectModel
+    {
+    }
+}
